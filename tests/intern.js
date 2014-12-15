0b2702c3f95e5686d36e1745772dc2414b65f812
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-12-15 11:52:51 +0100

    fixed remote testing

--- tests/intern.js
+++ tests/intern.js
@@ -7,37 +7,38 @@ define({
 
 	proxyPort: 9000,
 
-	proxyUrl: "http://localhost:9000/",
+	proxyUrl: "http://127.0.0.1:9000/",
 
 	environments: [
-		{browserName: "internet explorer", version: "9", platform: "Windows 7", name: "decor"},
-		{browserName: "internet explorer", version: "10", platform: "Windows 8", name: "decor"},
-		{browserName: "internet explorer", version: "11", platform: "Windows 8.1", name: "decor"},
-		{browserName: "firefox", version: "25", platform: [/*"OS X 10.6",*/ "Linux", "Windows 7"], name: "decor"},
-		{browserName: "chrome", version: "", platform: [/*"OS X 10.6", */ "Linux", "Windows 7"], name: "decor"},
-		{browserName: "safari", version: "6", platform: "OS X 10.8", name: "decor"},
-		{browserName: "safari", version: "7", platform: "OS X 10.9", name: "decor"},
+		{ browserName: "internet explorer", version: "11", platform: "Windows 8.1", requireWindowFocus: "true", name : "decor"},
+		{ browserName: "internet explorer", version: "10", platform: "Windows 8", requireWindowFocus: "true", name : "decor"},
+		{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
+		{ browserName: "firefox", version: "31", platform: [ /*"OS X 10.6", "Linux",*/ "Windows 7" ], name : "decor"},
+		{ browserName: "chrome", version: "32", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ], name : "decor"},
+		{ browserName: "safari", version: "7", platform: [ "OS X 10.9" ], name : "decor"}
+		//{browserName: "safari", version: "6", platform: "OS X 10.8", name: "decor"},
 		// {browserName: "android", platform: "Linux", version: "4.1", name: "decor"},
 		// {browserName: "android", platform: "Linux", "device-type": "tablet", version: "4.1", name: "decor"},
 		// {browserName: "android", platform: "Linux", version: "4.1", name: "decor"},
 		// {browserName: "android", platform: "Linux", "device-type": "tablet", version: "4.0", name: "decor"},
 		// {browserName: "android", platform: "Linux", version: "4.0", name: "decor"},
 		// Non-empty selenium-version causes "browser failed to start" error for unknown reason
-		{browserName: "iphone", platform: "OS X 10.9", version: "7",
-			"device-orientation": "portrait", "selenium-version": "", name: "decor"},
-		{browserName: "ipad", platform: "OS X 10.9", version: "7",
-			"device-orientation": "portrait", "selenium-version": "", name: "decor"},
+		//{browserName: "iphone", platform: "OS X 10.9", version: "7",
+			//"device-orientation": "portrait", "selenium-version": "", name: "decor"},
+		//{browserName: "ipad", platform: "OS X 10.9", version: "7",
+			//"device-orientation": "portrait", "selenium-version": "", name: "decor"},
 		// {browserName: "iphone", platform: "OS X 10.8", version: "6.0",
 		//	"device-orientation": "portrait", "selenium-version": "", name: "decor"},
 		// {browserName: "ipad", platform: "OS X 10.8", version: "6.0",
 		//	"device-orientation": "portrait", "selenium-version": "", name: "decor"},
-		{browserName: "iphone", platform: "OS X 10.8", version: "6.1",
-			"device-orientation": "portrait", "selenium-version": "", name: "decor"},
-		{browserName: "ipad", platform: "OS X 10.8", version: "6.1",
-			"device-orientation": "portrait", "selenium-version": "", name: "decor"}
+		//{browserName: "iphone", platform: "OS X 10.8", version: "6.1",
+			//"device-orientation": "portrait", "selenium-version": "", name: "decor"},
+		//{browserName: "ipad", platform: "OS X 10.8", version: "6.1",
+			//"device-orientation": "portrait", "selenium-version": "", name: "decor"}
 	],
 
 	maxConcurrency: 3,
+	tunnel: "SauceLabsTunnel",
 
 	// Maximum duration of a test, in milliseconds
 	TEST_TIMEOUT: 300000, // 5 minutes
